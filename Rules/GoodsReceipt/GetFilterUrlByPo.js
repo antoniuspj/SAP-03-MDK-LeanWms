9d66ee5a1017652fcb4fbe960b62643f67fe1494
antoniuspj <antoniuspj.04@gmail.com>
2020-06-03 09:31:04 +0000

    change filter po

--- Rules/GoodsReceipt/GetFilterUrlByPo.js
+++ Rules/GoodsReceipt/GetFilterUrlByPo.js
@@ -5,7 +5,7 @@ var var_value, clientAPI, var_po, url;
  */
 export default function GetFilterUrlByPo(clientAPI) {
 	//
-	//
+	// pj
 	var_po = clientAPI.evaluateTargetPath('#Page:GoodsReceiptList/#Control:InputPoNumber/#Value');
 	var_value = clientAPI.evaluateTargetPath('#Application/#ClientData/#Property:DeviceId');
 	url = "$filter=(Ponumber eq '" + var_po + "' and Mdksysid eq '" + var_value + "')";
